bd8aa005f6d7e61d84d1100b92e43bc5648271ad
ojov <osamudiamen.ojo@simplifysynergy.com>
2025-10-25 12:44:12 +0100

    Add README with API details; update .gitignore to exclude target directory

--- src/main/java/com/ojo/profilehng/ProfilehngApplication.java
+++ src/main/java/com/ojo/profilehng/ProfilehngApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfilehngApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProfilehngApplication.class, args);
     }
-
 }
